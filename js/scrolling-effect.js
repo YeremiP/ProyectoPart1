17cb88eff045fd727deb563b589daa3d8b14cc51
YeremiP <jeremypaul476@gmail.com>
2024-12-11 15:27:28 +0000

    Subido sin error

--- js/scrolling-effect.js
+++ js/scrolling-effect.js
@@ -2,6 +2,7 @@ let bg = document.getElementById("bg");
 let mountain = document.getElementById("mountain");
 let moon = document.getElementById("moon");
 let road = document.getElementById("road");
+let text = document.getElementById("text");
 
 window.addEventListener('scroll', function () {
   var value = window.scrollY;
@@ -9,7 +10,6 @@ window.addEventListener('scroll', function () {
   bg.style.top = value * 0.5 + 'px';
   moon.style.left = value * 0.5 + 'px';
   mountain.style.top = value * 0.15 + 'px';
-  road.style.top = value * 0.15 + 'px';
   road.style.top = value * 0.15 + 'px';
   text.style.top = value * 1 + 'px';
 })
--- js/show-data.js
+++ js/show-data.js
@@ -1,39 +1,3 @@
-// function loadCars() {
-//   const carList = JSON.parse(localStorage.getItem("carList")) || [];
-//   const carTable = document.getElementById("car-table");
-//   carTable.innerHTML = "";
-
-//   carList.forEach((car, index) => {
-//     const row = document.createElement("tr");
-//     row.innerHTML = `
-//       <td>${car.type}</td>
-//       <td>${car.model}</td>
-//       <td>${car.specifications}</td>
-//       <td>$${car.price}</td>
-//       <td>
-//         <button onclick="editCar(${index})">Editar</button>
-//         <button onclick="deleteCar(${index})">Eliminar</button>
-//       </td>
-//       `;
-//     carTable.appendChild(row);
-//   });
-// }
-
-// function editCar(index) {
-//   localStorage.setItem("editCarId", index);
-//   window.location.href = "form.html";
-// }
-
-// function deleteCar(index) {
-//   const carList = JSON.parse(localStorage.getItem("carList")) || [];
-//   carList.splice(index, 1);
-//   localStorage.setItem("carList", JSON.stringify(carList));
-//   loadCars();
-// }
-
-// document.addEventListener("DOMContentLoaded", loadCars);
-
-
 function loadCars() {
   const carList = JSON.parse(localStorage.getItem("carList")) || [];
   const carTable = document.getElementById("car-table");
